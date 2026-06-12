abb6e14524c902a1511ce6e7e295f916ecd22d5f
Gabriel Sa <gabriel10043@live.com>
2026-06-12 14:58:37 -0300

    fix UserCreateResponse bug
    - bug caused UserCreateResponse to swap email and username

--- backend/Dtos/UsersDto.cs
+++ backend/Dtos/UsersDto.cs
@@ -2,5 +2,5 @@ namespace Dto;
 
 public record UserCreateRequest(string email, string username, string password);
 
-public record UserData(int id, string username, string email, DateTime createdAt);
+public record UserData(int id, string email, string username, DateTime createdAt);
 public record UserCreateResponse(UserData data);
