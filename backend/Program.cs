89b0f2e647faf2ded1f14f3c50bb18f23e674e6e
Gabriel Sa <gabriel10043@live.com>
2026-06-12 18:34:17 -0300

    update JwtBearer options
    - update JwtBearer initialization options
    - include authentication support on swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 using Service;
 using Data;
@@ -10,9 +13,28 @@ builder.Services.Configure<JwtSettings>(
 		builder.Configuration.GetSection("JwtSettings")
 		);
 // Add services to the container.
+var jwtSettings = builder.Configuration
+    .GetSection("JwtSettings")
+    .Get<JwtSettings>();
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
-			opt => builder.Configuration.Bind("JwtSettings", opt));
+	.AddJwtBearer(opt =>
+	{
+		opt.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(
+					Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+
+			ValidateIssuer = true,
+			ValidIssuer = jwtSettings.Issuer,
+
+			ValidateAudience = true,
+			ValidAudience = jwtSettings.Audience,
+
+			ValidateLifetime = false
+		};
+	});
 
 builder.Services.AddDbContextPool<PandoraContext>(opt =>
 		opt.UseNpgsql(builder.Configuration.GetConnectionString("PandoraContext")));
@@ -23,7 +45,32 @@ builder.Services.AddScoped<AuthService>();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+		{
+			opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+					{
+						Name = "Authorization",
+						Type = SecuritySchemeType.Http,
+						Scheme = "bearer",
+						BearerFormat = "JWT",
+						In = ParameterLocation.Header,
+						Description = "Enter your JWT token"
+					});
+			opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+			{
+				{
+					new OpenApiSecurityScheme
+					{
+						Reference = new OpenApiReference
+						{
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+						}
+					},
+					Array.Empty<string>()
+				}
+			});
+	});
 
 var app = builder.Build();
 
