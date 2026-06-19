aabc9977a066316cec10fc2a0a0dbbe702e43439
Gabriel Sa <gabriel10043@live.com>
2026-06-19 18:21:51 -0300

    add DeleteAuthor tests

--- tests/pandora.tests/AuthorsServiceTests.cs
+++ tests/pandora.tests/AuthorsServiceTests.cs
@@ -52,4 +52,74 @@ public class AuthorsServiceTests : IClassFixture<DbFixture>
 
 		tx.Rollback();
 	}
+
+	[Fact]
+	public void DeleteAuthor_ShouldRemove()
+	{
+		var ctx = TestDbFactory.Create();
+		var service = new AuthorsService(ctx);
+		var bService = new BooksService(ctx);
+
+		using var tx = ctx.Database.BeginTransaction();
+
+		Author author = service.CreateAuthor("test author");
+
+		service.DeleteAuthor(author.Id);
+		Assert.Equal(0, ctx.Authors.Count());
+
+		tx.Rollback();
+	}
+
+	[Fact]
+	public void DeleteAuthor_ShouldDeleteAuthorBooks()
+	{
+		var ctx = TestDbFactory.Create();
+		var service = new AuthorsService(ctx);
+		var bService = new BooksService(ctx);
+
+		using var tx = ctx.Database.BeginTransaction();
+
+		Author author = service.CreateAuthor("test author");
+
+		List<int> authorsIds = new();
+
+		authorsIds.Add(author.Id);
+
+		bService.CreateBook("test book 1", "test description 1", "test isbn 1", 0, authorsIds);
+		bService.CreateBook("test book 2", "test description 2", "test isbn 2", 0, authorsIds);
+		bService.CreateBook("test book 3", "test description 3", "test isbn 3", 0, authorsIds);
+		bService.CreateBook("test book 4", "test description 4", "test isbn 4", 0, authorsIds);
+
+		service.DeleteAuthor(author.Id);
+
+		Assert.Equal(0, ctx.AuthorBooks.Count());
+		Assert.Equal(0, ctx.Books.Count());
+		tx.Rollback();
+	}
+
+	[Fact]
+	public void DeleteAuthor_ShouldNotDeleteSharedBooks()
+	{
+		var ctx = TestDbFactory.Create();
+		var service = new AuthorsService(ctx);
+		var bService = new BooksService(ctx);
+
+		using var tx = ctx.Database.BeginTransaction();
+
+		Author author1 = service.CreateAuthor("test author 1");
+		Author author2 = service.CreateAuthor("test author 2");
+
+		List<int> authorsIds = new();
+
+		authorsIds.Add(author1.Id);
+		authorsIds.Add(author2.Id);
+
+		bService.CreateBook("test shared book 1", "test description 1", "test isbn 1", 0, authorsIds);
+
+		service.DeleteAuthor(author1.Id);
+
+		Assert.Equal(1, ctx.AuthorBooks.Count());
+		Assert.Equal(1, ctx.Books.Count());
+		tx.Rollback();
+	}
 }
